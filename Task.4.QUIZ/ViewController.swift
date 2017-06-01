0d7f46bf779ea4c2ba089502cf51952bb2321c66
stud <stud@a130-05.local>
2017-06-01 11:29:40 +0200

    Initial Commit

---
+++ Task.4.QUIZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task.4.QUIZ
+//
+//  Created by stud on 01/06/2017.
+//  Copyright © 2017 stud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
